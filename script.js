09c1cfc7940445b449fd31dd724259153a7762f3
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-08-22 19:35:15 +0800

    fix overlay navigation

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+var body = document.getElementsByTagName("body")[0],
+  sidebar = document.getElementById("mySidenav");
+
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  sidebar.style.display = "block";
+  sidebar.style.width = "100%";
+  body.style.overflow = "hidden";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  sidebar.style.width = "0";
+  body.style.overflow = "auto";
 }
+
+// sidebar.onmouseover = function () {
+
+// };
+
+// sidebar.onmouseout = function () {
+//   body.style.overflow = "auto";
+// };
